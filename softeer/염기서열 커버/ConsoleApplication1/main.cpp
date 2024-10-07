9fd19a4127e2396db09fb4182d8508369323a408
Tamy <dksgnsrb@gmail.com>
2024-10-07 20:46:34 +0900

    2024-10-042024-10-04

--- softeer/염기서열 커버/ConsoleApplication1/main.cpp
+++ softeer/염기서열 커버/ConsoleApplication1/main.cpp
@@ -1,82 +1,60 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cmath>
+#include <cstring>
 using namespace std;
 
-char list[16][21];
-int dp[21][21], check[16];
+int n, m;
+char list[16][21], super_dna[2000][21];
+
+void merge(int index, char a[], char b[])
+{
+    int i;
+    char ret[21];
+    if (b == "") {
+        super_dna[index] = "";
+        return;
+    }
+    for (i = 0; i < m; i++) {
+        if (a[i] == '.' && b[i] != '.')
+            ret[i] = b[i];
+        else if (a[i] != '.' && b[i] == '.')
+            ret[i] = a[i];
+        else if (a[i] == '.' && b[i] == '.')
+            ret[i] = '.';
+        else {
+            ret = "";
+            break;
+        }
+    }
+    super_dna[index] = ret;
+}
+
+void gen_super(int index)
+{
+    int i, tmp = index, loc = 0;
+    while (tmp % 2 == 0) {
+        loc++;
+        tmp /= 2;
+    }
+    cout << "index:" << index << "\n" << list[loc + 1] << "\n" << super_dna[index - int(pow(2, loc))] << endl;
+    merge(index, list[loc + 1], super_dna[index - int(pow(2, loc - 1))]);
+}
 
 int main()
 {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int n, m, i, j, k, flag, ans, cnt = 0;
-	cin >> n >> m;
-	ans = n;
-	for (i = 1; i <= n; i++)
-		cin >> list[i];
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int i, j, t;
+    cin >> n >> m;
+    for (i = 1; i <= n; i++)
+        cin >> list[i];
 
-	for (i = 1; i < n; i++) {
-		for (j = i + 1; j <= n; j++) {
-			flag = 0;
-			for (k = 0; k < m; k++) {
-				if (list[i][k] == '.' || list[j][k] == '.' || list[i][k] == list[j][k])
-					continue;
-				flag = 1;
-				break;
-			}
-			if (!flag) {
-				dp[i][j] = 1;
-				dp[j][i] = 1;
-			}
-		}
-	}
+    t = pow(2, n) - 1;
 
-	for (i = 1; i < n; i++) {
-		for (j = i + 1; j <= n; j++) {
-			if (dp[i][j]) {
-				if (!check[i] && !check[j]) {
-					ans--;
-					cnt++;
-					check[i] = cnt;
-					check[j] = cnt;
-				}
-				else if (check[i] && !check[j]) {
-					flag = 0;
-					for (k = 1; k <= n; k++) {
-						if (check[i] == check[k]) {
-							if (!dp[j][k]) {
-								flag = 1;
-								break;
-							}
-						}
-					}
-					if (!flag) {
-						ans--;
-						check[j] = check[i];
-					}
-					else
-						check[j] = ++cnt;
-				}
-				else if (!check[i] && check[j]) {
-					flag = 0;
-					for (k = 1; k <= n; k++) {
-						if (check[j] == check[k]) {
-							if (!dp[i][k]) {
-								flag = 1;
-								break;
-							}
-						}
-					}
-					if (!flag) {
-						ans--;
-						check[i] = check[j];
-					}
-					else
-						check[i] = ++cnt;
-				}
-			}
-		}
-	}
-
-	cout << ans;
-	return 0;
+    for (i = 0; i < m; i++)
+        super_dna[0][i] = '.';
+    for (i = 1; i <= t; i++) {
+        gen_super(i);
+        cout << super_dna[i];
+    }
+    return 0;
 }
